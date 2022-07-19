2f51b0a8ee8a6cad267aaa87d33ef8263803d6b7
Kier Julian Genoguin <kierj.genoguin@gmail.com>
2022-07-19 12:22:28 +0800

    Solution for notString problem.
    Signed-off-by: Kier Julian Genoguin <kierj.genoguin@gmail.com>

---
+++ java/warmup-1/NotString.java
@@ -0,0 +1,9 @@
+/*
+Given a string, return a new string where "not " has been added to the front. 
+However, if the string already begins with "not", return the string unchanged. 
+Note: use .equals() to compare 2 strings.
+*/
+
+public String notString(String str) {
+  return str.startsWith("not") ? str : "not " + str;
+}
