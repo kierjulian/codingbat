bb3f10c9b88c6ab6d1c987369aa241bc14ceb7b6
Kier Julian Genoguin <kierj.genoguin@gmail.com>
2022-08-24 00:09:03 +0800

    Solution for delDel and mixStart problems.
    Signed-off-by: Kier Julian Genoguin <kierj.genoguin@gmail.com>

---
+++ java/warmup-1/DelDel.java
@@ -0,0 +1,18 @@
+/*
+Given a string, if the string "del" appears starting at index 1, return a string where that "del" has been deleted. 
+Otherwise, return the string unchanged.
+
+
+delDel("adelbc") → "abc"
+delDel("adelHello") → "aHello"
+delDel("adedbc") → "adedbc"
+*/
+public String delDel(String str) {
+  if (str.length() < 4) {
+    return str;
+  }
+  
+  return str.substring(1, 4).equals("del") 
+    ? str.substring(0, 1) + str.substring(4)
+    : str;
+}
