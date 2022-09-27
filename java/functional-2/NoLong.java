42a848ebd28f0bd5907410ca9a356d7f9accadb7
Kier Julian Genoguin <kierj.genoguin@gmail.com>
2022-09-27 23:39:32 +0800

    Implemented solution for noLong problem.
    Signed-off-by: Kier Julian Genoguin <kierj.genoguin@gmail.com>

---
+++ java/functional-2/NoLong.java
@@ -0,0 +1,13 @@
+/*
+Given a list of strings, return a list of the strings, omitting any string length 4 or more.
+
+
+noLong(["this", "not", "too", "long"]) → ["not", "too"]
+noLong(["a", "bbb", "cccc"]) → ["a", "bbb"]
+noLong(["cccc", "cccc", "cccc"]) → []
+*/
+public List<String> noLong(List<String> strings) {
+  return strings.stream()
+    .filter(x -> x.length() < 4)
+    .collect(Collectors.toList());
+}
