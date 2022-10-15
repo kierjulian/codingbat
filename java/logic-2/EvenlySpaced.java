80678a0820ed34db39cabbf2156c82102417b8c7
Kier Julian Genoguin <kierj.genoguin@gmail.com>
2022-10-16 00:01:03 +0800

    Implemented solution for evenlySpaced problem.
    Signed-off-by: Kier Julian Genoguin <kierj.genoguin@gmail.com>

---
+++ java/logic-2/EvenlySpaced.java
@@ -0,0 +1,14 @@
+/*
+Given three ints, a b c, one of them is small, one is medium and one is large. 
+Return true if the three values are evenly spaced, so the difference between small and medium is the same as the difference between medium and large.
+
+
+evenlySpaced(2, 4, 6) → true
+evenlySpaced(4, 6, 2) → true
+evenlySpaced(4, 6, 3) → false
+*/
+public boolean evenlySpaced(int a, int b, int c) {
+  List<Integer> intList = Arrays.asList(a, b, c);
+  Collections.sort(intList);
+  return intList.get(1) - intList.get(0) == intList.get(2) - intList.get(1);
+}
