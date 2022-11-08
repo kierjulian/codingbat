302b4b291d6531db09cc4defcc49d6e177e1ae8c
Kier Julian Genoguin <kierj.genoguin@gmail.com>
2022-11-08 20:08:09 +0800

    Implemented solutions for different problems.
    Signed-off-by: Kier Julian Genoguin <kierj.genoguin@gmail.com>

---
+++ java/functional-2/NoYY.java
@@ -0,0 +1,14 @@
+/*
+Given a list of strings, return a list where each string has "y" added at its end, omitting any resulting strings that contain "yy" as a substring anywhere.
+
+
+noYY(["a", "b", "c"]) → ["ay", "by", "cy"]
+noYY(["a", "b", "cy"]) → ["ay", "by"]
+noYY(["xx", "ya", "zz"]) → ["xxy", "yay", "zzy"]
+*/
+public List<String> noYY(List<String> strings) {
+  return strings.stream()
+    .map(string -> string + "y")
+    .filter(string -> !string.contains("yy"))
+    .collect(Collectors.toList());
+}
---
+++ java/string-3/EqualIsNot.java
@@ -0,0 +1,15 @@
+/*
+Given two strings, base and remove, return a version of the base string where all instances of the remove string have been removed (not case sensitive). 
+You may assume that the remove string is length 1 or more. 
+Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
+
+
+withoutString("Hello there", "llo") → "He there"
+withoutString("Hello there", "e") → "Hllo thr"
+withoutString("Hello there", "x") → "Hello there"
+*/
+public boolean equalIsNot(String str) {
+  int is = (str.length() - str.replace("is", "").length()) / 2;
+  int not = (str.length() - str.replace("not", "").length()) / 3;
+  return is == not;
+}
